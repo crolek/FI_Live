2faafbd94a0dda419d6ff9959748d5ca9e9934e5
Steven Nelson <stevennelson@Inuvo-Dev-MacBook-Air.local>
2014-06-18 20:45:46 -0700

    First stab at creating a new assessment and returning the ID

--- router.js
+++ router.js
@@ -1,1 +1,16 @@
 var routes = module.exports = require('express').Router();
+routes.get('/assessment/create', function(req, res){
+	var traitify = require("traitify");
+	// live url
+	//traitify.setHost("api.traitify.com");
+
+	// testing url
+	traitify.setHost("api-sandbox.traitify.com");
+
+	traitify.setVersion("v1");
+	traitify.setPrivateKey("rg8ogn28rfjdcr0708tm3bgmdi");
+	traitify.createAssessment("deck-id", function(assessment){
+	  // Use assessment here.
+	  res.send(assessment);
+	});
+});
