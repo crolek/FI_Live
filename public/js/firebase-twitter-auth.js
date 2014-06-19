9144a8059fbc301d546f0118787b14c61d59bf83
Chuck Rolek <chuck@chuckrolek.com>
2014-06-18 23:04:51 -0500

    oh sherpa we killed a goat

---
+++ public/js/firebase-twitter-auth.js
@@ -0,0 +1,16 @@
+app = angular.module('app', ['firebase']);
+
+app.controller('AuthCtrl', [
+	'$scope', '$rootScope', '$firebaseAuth', function($scope, $rootScope, $firebaseAuth) {
+		var ref = new Firebase('https://torid-fire-513.firebaseio.com/');
+		$rootScope.auth = $firebaseAuth(ref);
+		
+		$scope.signIn = function () {
+			$rootScope.auth.$login("twitter").then(function(user) {
+				console.log("sherpa we failed");
+			}, function(error) {
+				console.log(error);
+			});
+		}
+	}
+]);
