d6c1e6b6e4bac4909141f0d9ac4384ae87a99cbd
mattbalmer <signup@mattbalmer.com>
2014-06-18 22:52:50 -0700

    stuff

--- public/ng/controllers/results.js
+++ public/ng/controllers/results.js
@@ -1,3 +1,7 @@
 angular.module('frenemy').controller('results', function($scope, $routeParams) {
     $scope.side = $routeParams.side;
+
+    $scope.tweet = function() {
+        window.location.href = 'http://twitter.com/home?status=' + encodeURI("I'm a "+$scope.side+" superhero! Find out if we're friends or enemies at http://frenemy.laet.us");
+    }
 });
